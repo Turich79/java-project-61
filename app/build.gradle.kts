65891107c936af092651212757c89a3fe7268295
Turich79 <turich79@mail.ru>
2024-05-02 21:09:01 +0700

    first

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,28 +1,19 @@
 plugins {
-    id("application")
     id("java")
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
-application {
-   mainClass.set("hexlet.code.App")
-}
-
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-
-tasks.getByName("run", JavaExec::class) {
-    standardInput = System.`in`
 }
