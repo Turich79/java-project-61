ac9aa7ad3c4cfea7ae6b28fe427b6cefcbe9b0fe
Turich79 <turich79@mail.ru>
2024-05-04 22:34:35 +0700

    project 1 mis 8

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Engine {
     private String name;
     private String numGame;
-    final int ITER = 3;
+    private final int iter = 3;
 
-    public Engine(String userName, String numberOfGame) {
+    public Engine(final String userName, final String numberOfGame) {
         this.name = userName;
         this.numGame = numberOfGame;
     }
@@ -37,13 +37,13 @@ public class Engine {
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
         int maxNum = 20;
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < ITER; i++) {
+        for (int i = 0; i < iter; i++) {
             int rand = (int) (Math.random() * maxNum);
             String resAnswer = rand % 2 == 0 ? "yes" : "no";
             System.out.println("Question: " + rand);
             System.out.print("Your answer: ");
             String answer = sc.nextLine();
-            if (!checkAnswer(resAnswer, answer)) {
+            if (incorrectAnswer(resAnswer, answer)) {
                 return;
             }
         }
@@ -56,13 +56,13 @@ public class Engine {
         int number2;
         int resAnswer;
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < ITER; i++) {
+        for (int i = 0; i < iter; i++) {
             number1 = (int) (Math.random() * 20);
             number2 = (int) (Math.random() * 20);
             resAnswer = Expr.expression(number1, number2, i);
             System.out.print("Your answer: ");
             int answer = sc.nextInt();
-            if (!checkAnswer(resAnswer, answer)) {
+            if (incorrectAnswer(resAnswer, answer)) {
                 return;
             }
         }
@@ -76,13 +76,13 @@ public class Engine {
         int resAnswer;
         int maxNum = 100;
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < ITER; i++) {
+        for (int i = 0; i < iter; i++) {
             number1 = (int) (Math.random() * maxNum);
             number2 = (int) (Math.random() * maxNum);
             resAnswer = Expr.returnGCD(number1, number2);
             System.out.print("Your answer: ");
             int answer = sc.nextInt();
-            if (!checkAnswer(resAnswer, answer)) {
+            if (incorrectAnswer(resAnswer, answer)) {
                 return;
             }
         }
@@ -93,11 +93,11 @@ public class Engine {
         System.out.println("What number is missing in the progression?");
         int resAnswer;
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < ITER; i++) {
+        for (int i = 0; i < iter; i++) {
             resAnswer = Expr.getNumberOfProgression();
             System.out.print("\nYour answer: ");
             int answer = sc.nextInt();
-            if (!checkAnswer(resAnswer, answer)) {
+            if (incorrectAnswer(resAnswer, answer)) {
                 return;
             }
         }
@@ -108,36 +108,36 @@ public class Engine {
         System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
         String resAnswer;
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < ITER; i++) {
+        for (int i = 0; i < iter; i++) {
             resAnswer = Expr.predPrimeNumber();
             System.out.print("\nYour answer: ");
             String answer = sc.nextLine();
-            if (!checkAnswer(resAnswer, answer)) {
+            if (incorrectAnswer(resAnswer, answer)) {
                 return;
             }
         }
         System.out.println("Congratulations, " + name + "!");
     }
 
-    private boolean checkAnswer(int resAnswer, int answer) {
+    private boolean incorrectAnswer(int resAnswer, int answer) {
         if (resAnswer != answer) {
             System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + resAnswer + "'.");
             System.out.println("Let's try again, " + name + "!");
-            return false;
+            return true;
         } else {
             System.out.println("Correct!");
-            return true;
+            return false;
         }
     }
 
-    private boolean checkAnswer(String resAnswer, String answer) {
+    private boolean incorrectAnswer(String resAnswer, String answer) {
         if (!answer.equals(resAnswer)) {
             System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + resAnswer + "'.");
             System.out.println("Let's try again, " + name + "!");
-            return false;
+            return true;
         } else {
             System.out.println("Correct!");
-            return true;
+            return false;
         }
     }
 }
