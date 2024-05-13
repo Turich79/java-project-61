16d763ba6e22da495a781ecc52fad85b368c2658
Turich79 <turich79@mail.ru>
2024-05-13 21:21:25 +0700

    correct 3_2

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -19,6 +19,9 @@ public final class GCD {
 
     private static int returnGCD(int number1, int number2) {
         int gcd = 1;
+        if (number1 == 0 || number2 == 0) {
+            return gcd;
+        }
         for (int i = 1; i <= number1 && i <= number2; i++) {
             if (number1 % i == 0 && number2 % i == 0) {
                 gcd = i;
